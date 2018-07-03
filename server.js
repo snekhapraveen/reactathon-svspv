caf039507f0d27a796cfab68a13fa542e890f2a2
snekhapraveen <40787283+snekhapraveen@users.noreply.github.com>
2018-07-03 15:56:55 +0000

    #CORS support
    #CORS support

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.all('/*', function(req, res, next) {
   next();
 });
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 app.get('/', function (req, res) {
     console.log("request received");
     res.send('Hello World');
